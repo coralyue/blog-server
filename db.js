fd5ecfc90b94bc69cfea676b0f0928b1def82bc1
coral3012 <coral3012@163.com>
2021-04-29 19:53:23 +0800

    修改数据库名称

--- db.js
+++ db.js
@@ -1,20 +1,12 @@
 const mongoose = require("mongoose");
-const { Book, BookCategory } = require("./models");
 
 mongoose
-  .connect("mongodb://localhost:27017/cat-shop", {
+  .connect("mongodb://localhost:27017/blog-server", {
     useNewUrlParser: true
   })
   .then(res => {
-    // console.log(res);
-    console.log("数据库连接成功");
-    Book.find({ id: "5def3ddcdf4b140e30dab5df" })
-      .populate({
-        path: "bookChapterCount"
-      })
-      .then(res => {
-        console.log(res);
-      });
+    //console.log(res);
+    console.log("数据库连接成功了！");
   })
   .catch(err => {
     console.log(err);
